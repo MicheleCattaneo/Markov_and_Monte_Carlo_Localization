Fixed misaligned axis between model and view. Implemented belief update for rotation and added some more plots.
Co-authored-by: MicheleCattaneo <cattami@usi.ch>

model/robot.py
@@ -10,7 +10,6 @@ import matplotlib.pyplot as plt
 
 
 class Robot(RobotBase):
-
     directions = [
         np.array([0, 1]),
         np.array([1, 1]),
@@ -22,12 +21,12 @@ class Robot(RobotBase):
         np.array([-1, 1])
     ]
 
-    def __init__(self, world, x, y, movement_model: MovementModelBase ,sensor: SensorBase) -> None:
+    def __init__(self, world, x, y, movement_model: MovementModelBase, sensor: SensorBase) -> None:
         self.set_position(x, y)
         self.sensor = sensor
 
         self.world = world
-        self.true_measurements = self.precompute_measurements(world.height, world.width)
+        self.true_measurements = self.precompute_measurements(world.width, world.height)
 
         self.belief = np.ones_like(self.true_measurements)
         self.belief[~self.world.walkable] = 0
@@ -35,18 +34,16 @@ class Robot(RobotBase):
 
         fig, ax = plt.subplots(figsize=(8, 4))
         sns.heatmap(self.world.walkable, ax=ax, linewidths=0.1)
-        # ax.invert_yaxis()
         fig.savefig('./plots/walkable_belief_up.png')
 
         fig, ax = plt.subplots(figsize=(8, 4))
-        sns.heatmap(self.belief[:,:,0], ax=ax, linewidths=0.1)
-        # ax.invert_yaxis()
+        sns.heatmap(self.belief[:, :, 0], ax=ax, linewidths=0.1)
         fig.savefig('./plots/first_belief_up.png')
 
-        fig, ax = plt.subplots(figsize=(8, 4))
-        sns.heatmap(np.nan_to_num(self.true_measurements.max(axis=2), posinf=-1), ax=ax, linewidths=0.1)
-        # ax.invert_yaxis()
-        fig.savefig('./plots/true_measurements.png')
+        for i in range(8):
+            fig, ax = plt.subplots(figsize=(8, 4))
+            sns.heatmap(self.true_measurements[:, :, i], ax=ax, linewidths=0.1)
+            fig.savefig(f'./plots/true_measurements_{i}.png')
 
         self.movement_model = movement_model
 
@@ -54,32 +51,32 @@ class Robot(RobotBase):
 
     # region Markov
 
-    def precompute_measurements(self, height: int, width: int):
-        means = np.zeros((height, width, 8))
+    def precompute_measurements(self, width: int, height: int):
+        orientations = 8
+        means = np.zeros((width, height, orientations))
 
-        for i in range(means.shape[0]):
-            for j in range(means.shape[1]):
-                for d in range(means.shape[2]):
+        for i in range(width):
+            for j in range(height):
+                for d in range(orientations):
                     if not self.world.walkable[i, j]:
                         continue
 
                     means[i, j, d] = self.sensor.sense(np.array([i, j]) + ROBOT_SIZE, self.Direction(d))
-
+        means.setflags(write=False)
         return means
-    
+
     def measurement_probability(self, measurement: float) -> float:
-        '''
-        Given a measurement, returns the probability 
+        """
+        Given a measurement, returns the probability
         of being at a position and measuring the given measurement.
-        '''
+        """
         return np.isclose(self.true_measurements, measurement).astype('int')
-    
+
     def measurement_probability_uncertain(self, measurement: float, location: tuple) -> float:
         pass
 
     def see(self, measurement):
         self.belief = self.measurement_probability(measurement) * self.belief
-        print(self.belief.sum(), 'sum belief')
         self.belief = self.belief / self.belief.sum()
 
         # assert np.isclose(self.belief.sum(), 1.), f'sum is not 1 {self.belief.sum()}'
@@ -88,11 +85,33 @@ class Robot(RobotBase):
         try:
             filter = self.movement_model.get_filter(action)
             for i in range(filter.shape[-1]):
-                self.belief[:,:,i] = convolve2d(self.belief[:,:,i], filter[:,:,i], mode='same')
+                self.belief[:, :, i] = convolve2d(self.belief[:, :, i], filter[:, :, i], mode='same', boundary="wrap")
+            return
         except InvalidActionException:
             pass
+        direction = 1 if action == action.TURN_RIGHT else -1
+        self.belief = np.roll(self.belief, shift=direction, axis=2)
 
     # endregion
+
+    def plot_8_orientations(self):
+        fig, ax = plt.subplots(3, 3, figsize=(12, 12))
+
+        row, col = 0, 0
+        j = 0
+        for _ in range(9):
+            if row == 1 and col == 1:
+                col += 1
+                continue
+            sns.heatmap(self.belief[:, :, j].T, ax=ax[row, col], linewidths=0.1)
+            ax[row, col].invert_yaxis()
+            col += 1
+            j += 1
+            if col % 3 == 0:
+                col = 0
+                row += 1
+        fig.savefig("plots/probs.png")
+        fig.show()
 
     # region Move
 
@@ -109,23 +128,23 @@ class Robot(RobotBase):
             new_pos = self.position + Robot.directions[move_dir]
 
             # get i,j coordinates and check whether that tile is occupied by an object (obstacle)
-            if self.world.walkable[tuple(new_pos.astype(int))]:
-                self.set_position(new_pos)
-
-
-            print(f'Current position: {self.position * TILE_SIZE}')
+            if not self.world.walkable[tuple(new_pos.astype(int))]:
+                return
+            self.set_position(new_pos)
 
         reading = self.sensor.sense(self.position + ROBOT_SIZE, self.orientation)
-        print(reading, 'reading')
         assert reading == self.true_measurements[self.state], "True measurements are incorrect"
 
         self.act(action)
         self.see(reading)
 
-        fig, ax = plt.subplots(figsize=(8, 4))
-        sns.heatmap(self.belief[:,:,0], ax=ax, linewidths=0.1)
-        ax.invert_yaxis()
-        fig.savefig('./plots/prob_belief_up.png')
+        self.plot_8_orientations()
+
+        # fig, ax = plt.subplots(figsize=(8, 4))
+        # sns.heatmap(self.belief[:, :, (8-self.orientation.value)%8].T, ax=ax, linewidths=0.1)
+        # ax.invert_yaxis()
+        # fig.savefig('./plots/prob_belief_up.png')
+        # fig.show()
 
         self.on_move.notify()
 

model/sensor.py
@@ -39,7 +39,7 @@ class LaserSensor(SensorBase):
 
     def sense(self, xy: np.ndarray, direction: RobotBase.Direction, raw: bool = False) -> np.ndarray:
         self.laser_beam_obj.delete()
-        gy, gx = xy * TILE_SIZE
+        gx, gy = xy * TILE_SIZE
 
         mass_center = shapely.Point((gx, gy))
         self.laser_beam_obj = self.create_laser_beam_object(gx, gy, direction)
@@ -48,7 +48,10 @@ class LaserSensor(SensorBase):
         closest_obj = None
 
         for object in self.world.objects:
-            intersection = object.shapely_shape.exterior.intersection(self.laser_beam_obj.shapely_shape)
+            if object.shapely_shape.geom_type == "LineString":
+                intersection = object.shapely_shape.intersection(self.laser_beam_obj.shapely_shape)
+            else:
+                intersection = object.shapely_shape.exterior.intersection(self.laser_beam_obj.shapely_shape)
             if not intersection.is_empty:
                 if intersection.geom_type == "LineString":
                     intersection = intersection.boundary